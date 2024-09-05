fce45c3874bf213ffdbb562600fe3c34651ae9f1
ramya-naik <ramya.naik@soulunileaders.com>
2024-09-05 11:25:37 +0530

    close food donation API, frontend API List update

--- frontend/src/utils/apiList.js
+++ frontend/src/utils/apiList.js
@@ -3,11 +3,33 @@ const api = {
         url: "/food/addFoodDonationRequest",
         method: "post"
         /**
+         * in req.body
          * 1. foodItemsArray [Object]:-
          *      foodItemsArray = [{ foodName, foodCategory, quantity, unit, expirationDate, imageData }, ...]
          * 2. receiverId integer
          */
     },
+    VIEW_FOOD_DONATION_LIST: {
+        url: "/food/viewFoodDonationList",
+        method: "post"
+        /**
+         * in req.body - page_size, page_number, timeLimit, userLatitude, userLongitude, distanceRange, foodType, givenReq
+         */
+    },
+    VIEW_FOOD_DONATION_BY_ID: {
+        url: "/food/viewFoodDonationById",
+        method: "get",
+        /**
+         * in req.params - foodListingId
+         */
+    },
+    CLOSE_FOOD_DONATION: {
+        url: "/food/closeFoodDonation",
+        method: "put",
+        /**
+         * in req.body - foodListingId
+         */
+    }
 }
 
 export default api;
